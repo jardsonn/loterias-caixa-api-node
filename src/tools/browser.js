722fdac9aa953d792cded63b8091e797c590ff14
Jardson Costa <jardsoncostasilva31@gmail.com>
2021-10-20 19:55:36 -0300

    add args in call puppeteer.launch

--- src/tools/browser.js
+++ src/tools/browser.js
@@ -2,9 +2,13 @@ const puppeteer = require('puppeteer')
 const cheerio = require('cheerio')
 
 async function startBrowser(loteria = '') {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+         args: ['--no-sandbox'] 
+    })
     const page = await browser.newPage()
-    await page.goto(`http://loterias.caixa.gov.br/wps/portal/loterias/landing/${loteria}`)
+    await page.goto(
+        `http://loterias.caixa.gov.br/wps/portal/loterias/landing/${loteria}`
+    )
     await page.setCacheEnabled(false)
     const sourceCodeHtml = await page.content()
     const pageData = cheerio.load(sourceCodeHtml)
